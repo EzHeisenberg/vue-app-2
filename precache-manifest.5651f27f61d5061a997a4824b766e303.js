6ec3d811787568c9ccc7f82d39fc3524be50f899
EzHeisenberg <lucaporal@live.fr>
2021-03-15 14:29:07 +0100

    deploy

---
+++ precache-manifest.5651f27f61d5061a997a4824b766e303.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c35aa0925060270db6ca",
+    "url": "/vue-app-2/css/app.450eb6dd.css"
+  },
+  {
+    "revision": "bf46b292d45b3c41ac44e8879d1884c4",
+    "url": "/vue-app-2/index.html"
+  },
+  {
+    "revision": "c35aa0925060270db6ca",
+    "url": "/vue-app-2/js/app.a1e58963.js"
+  },
+  {
+    "revision": "7c6eb50e8bd384b3298b",
+    "url": "/vue-app-2/js/chunk-vendors.fd0661cf.js"
+  },
+  {
+    "revision": "a2d5b338f77ef563188278d1c66fc3a2",
+    "url": "/vue-app-2/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-app-2/robots.txt"
+  }
+]);
